49bf0e052db0891b825b207b9fe6a5c0edf2313c
Aaron Low <Hawaiian_spawn@live.com>
2017-06-28 23:02:01 -0600

    Daily Commit

---
+++ Assets/BUMP/Tree/TreeSmack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSmack : MonoBehaviour {
+    public Animator Anim;
+    public int Hits;
+	// Use this for initialization
+	void Start () {
+        Anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseDown()
+    {
+        Hits++;
+        Anim.SetInteger("HitType", Random.Range(0,2));
+        Anim.Play("Hit");
+        if (Hits > 20f)
+        {
+            Anim.Play("TreeCut");
+            Destroy(this);
+        }
+        
+    }
+}

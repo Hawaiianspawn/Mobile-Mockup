caa2359a9c0f08134bd51da0c969a28ea8c96001
Aaron Low <Hawaiian_spawn@live.com>
2017-07-11 12:46:12 -0600

    build to work with
    Build to work with for class

---
+++ Assets/Scripts/UI/TransitionManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionManager : MonoBehaviour {
+    protected Animator Anim;
+    protected SpriteRenderer[] Children;
+    bool OnOff;
+	// Use this for initialization
+	void Start () {
+      //  DontDestroyOnLoad(this.gameObject);
+        Anim = GetComponent<Animator>();
+        Children = this.GetComponentsInChildren<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump")) {
+            if (OnOff)
+                endTransition();
+            else
+                startTranisiton();
+                }
+	}
+    void startTranisiton()
+    {
+        OnOff = true;
+
+        foreach (SpriteRenderer Sprite in Children)
+            if (!Sprite.enabled)
+                Sprite.enabled = true;
+        Anim.Play("Close");
+        Anim.SetFloat("_speed", 1f);
+    }
+    void endTransition()
+    {
+
+        OnOff = false;
+        Anim.SetFloat("_speed", -2.5f);
+        StartCoroutine(waitAndHide());
+    }
+
+    IEnumerator waitAndHide()
+    {
+        if(Children[0].enabled)
+            yield return new WaitForSeconds(3f);
+        foreach (SpriteRenderer Sprite in Children)
+            if (Sprite.enabled)
+                Sprite.enabled = false;
+
+    }
+}

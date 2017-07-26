5e1b83e1f35d1564566d6ed5d168fa79bd5cf794
Aaron Low <Hawaiian_spawn@live.com>
2017-07-25 21:18:40 -0700

    Back To Windows
    Miss

--- Assets/Scripts/Mechanics/SoundMechanic.cs
+++ Assets/Scripts/Mechanics/SoundMechanic.cs
@@ -1,8 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundMechanic : MonoBehaviour {
+
+
+	protected float completion;
+	public bool muteToWin;
+	public Image Background; 
+	public float Ratio;
+	public Text DebugText;
 
 	// Use this for initialization
 	void Start () {
@@ -14,4 +23,21 @@ public class SoundMechanic : MonoBehaviour {
       //  SendMessage("VolumeUp");
       //   SendMessage("VolumeDown");
 	}
+
+	public void volumeChange(float value){
+		Background.color = new Color(255 * value, 0, 0, 1);
+	}
+public void volumeUp(string value){
+		//completion  = value;
+		print (value);
+		if (!muteToWin && completion > .9f)
+			print ("win");
+}
+
+public void volumeDown(string value){
+	//completion = value;
+	if(muteToWin && completion < .1f)
+		print("win");
+
+}
 }
